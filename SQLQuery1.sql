643a3d1b43c624b25af5d839c56511b3e1919594
Harsh <harsh.21483@gmail.com>
2021-07-01 09:23:50 -0700

    [Harsh]Refactor:-Contact table is added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE AddressBook;
 USE AddressBook;
+
+create table Contact(
+firstName varchar(50),
+lastName varchar(50),
+address varchar(100),
+city varchar(100),
+state varchar(100),
+zip int,
+phoneNumber int,
+email varchar(60)
+);
