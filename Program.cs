af57e591dcea3e8ceb20da500e5a548e8ff3a99f
Harsh <harsh.21483@gmail.com>
2021-07-01 09:12:06 -0700

    [Harsh]Add:-AddressBook database is added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvAddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE AddressBook;
+USE AddressBook;
